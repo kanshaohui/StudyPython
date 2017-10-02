f54b7bbc1a5485f94131472cbd3b13d3e02476cb
kanshaohui <30545469@qq.com>
2017-10-02 23:08:22 +0800

    第三个版本

--- greeter.py
+++ greeter.py
@@ -2,4 +2,9 @@ def greet_user(username):
     """显示简单的问候语"""
     print("Hello " + username.title() + "!")
 
+def display_message():
+    """打印函数学习内容"""
+    print("这厮函数相关知识的学习")
+
+display_message()
 greet_user('jesse')
