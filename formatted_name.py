b3de628fcc701303ce369ded93b8065f413d6622
kanshaohui <30545469@qq.com>
2017-10-02 23:27:44 +0800

    第2个版本

--- formatted_name.py
+++ formatted_name.py
@@ -5,3 +5,15 @@ def get_formatted_name(first_name, last_name):
 
 musician = get_formatted_name('jimi', 'hendrix')
 print(musician)
+
+while True:
+    print("\nPlease tell me your name:")
+    print("(enter 'q' at any time to quit)")
+    f_name = input("First name :")
+    if f_name == 'q':
+        break
+    l_name = input("last name : ")
+    if l_name == 'q':
+        break
+    formatted_name = get_formatted_name(f_name, l_name)
+    print("\nHello " + formatted_name + " !")
