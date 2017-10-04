9a73fe8a03c70d1a4b459a78d9df563788275c57
kanshaohui <30545469@qq.com>
2017-10-05 01:00:35 +0800

    第1个版本

--- electric_car.py
+++ electric_car.py
@@ -33,6 +33,32 @@ class ElectricCar(Car):
     """电动汽车的独特之处"""
     def __init__(self, make, model, year):
         super().__init__(make, model, year)
+        self.battery = Battery()
+#    def describe_battery(self):
+#        """打印一条描述电瓶容量的消息"""
+#        print("This car has a  " + str(self.battery_size) + "-kWh battery.")
+
+class Battery():
+    """模拟电动汽车电瓶的简单尝试"""
+    def __init__(self, battery_size=70):
+        """初始化电瓶的属性"""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """打印描述电瓶容量的消息"""
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+    def get_range(self):
+        """打印电池的续航里程"""
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge."
+        print(message)
 
 my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
