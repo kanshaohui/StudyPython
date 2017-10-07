35e7853c8b57259f91dfb01851d35820236de2f6
kanshaohui <30545469@qq.com>
2017-10-07 23:55:45 +0800

    第1个版本

---
+++ my_car.py
@@ -0,0 +1,11 @@
+from car import Car
+
+my_new_car = Car('audi', 'a4', 2016)
+print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
+my_new_car.update_odometer(23)
+my_new_car.read_odometer()
+my_new_car.update_odometer(12)
+my_new_car.read_odometer()
+my_new_car.update_odometer(46)
+my_new_car.read_odometer()
