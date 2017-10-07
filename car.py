196c15a1f8291da114d758bf1702f72f3cdaf573
kanshaohui <30545469@qq.com>
2017-10-07 23:54:55 +0800

    模块化修改

--- car.py
+++ car.py
@@ -1,3 +1,5 @@
+"""一个可用于表示汽车的类"""
+
 class Car():
     """一次模拟汽车的简单尝试"""
     def __init__(self, make, model, year):
@@ -29,12 +31,31 @@ class Car():
         """将里程表增加指定的值"""
         self.odometer_reading += miles
 
-my_new_car = Car('audi', 'a4', 2016)
-print(my_new_car.get_descriptive_name())
-my_new_car.read_odometer()
-my_new_car.update_odometer(23)
-my_new_car.read_odometer()
-my_new_car.update_odometer(12)
-my_new_car.read_odometer()
-my_new_car.update_odometer(46)
-my_new_car.read_odometer()
+class ElectricCar(Car):
+    """电动汽车的独特之处"""
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery = Battery()
+#    def describe_battery(self):
+#        """打印一条描述电瓶容量的消息"""
+#        print("This car has a  " + str(self.battery_size) + "-kWh battery.")
+
+class Battery():
+    """模拟电动汽车电瓶的简单尝试"""
+    def __init__(self, battery_size=70):
+        """初始化电瓶的属性"""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """打印描述电瓶容量的消息"""
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+    def get_range(self):
+        """打印电池的续航里程"""
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge."
+        print(message)
